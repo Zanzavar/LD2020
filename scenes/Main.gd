b2b57700d83cfe8425382ce00a48850135ce2d7a
Aristos Pasalides <apasalides@axiomc.com>
2020-10-03 16:25:14 +0300

    Intro and World main structure.

---
+++ scenes/Main.gd
@@ -0,0 +1,17 @@
+extends Control
+
+var world = load("res://scenes/Game.tscn").instance()
+var gaming = true
+	
+func _input(event):
+	if event is InputEventScreenTouch:
+		if event.is_pressed(): Input.action_press("ui_accept")
+		else: Input.action_release("ui_accept")
+
+func _process(delta):
+	if Input.is_action_pressed("ui_accept") and !gaming:
+		add_child(world)
+		gaming = true
+
+func _on_Timer_timeout():
+	gaming = false
