54fdc7be0a3ba9af0ebd053441b4b612d2bf15df
Aristos Pasalides <apasalides@axiomc.com>
2020-10-04 10:21:41 +0300

    Made text in Intro to fade in.

--- scenes/Intro.gd
+++ scenes/Intro.gd
@@ -9,6 +9,7 @@ func _ready():
 
 func _process(delta):
 	if Input.is_action_pressed("ui_accept"):
+		Input.action_release("ui_accept")
 		if state == 0:
 			$AnimationPlayer.play("Text2")
 		elif state == 1:
@@ -16,7 +17,7 @@ func _process(delta):
 		else:
 			get_parent().add_child(world)
 			get_parent().remove_child(self)
+		state += 1
 			
 func _on_AnimationPlayer_animation_finished(anim_name):
-	state+=1
-	#$AnimationPlayer.play("TextFadeIn")
+	pass
