1fe8bf981d7234eaed596703349da03cab2a97f7
Aristos Pasalides <rtsketo@gmail.com>
2020-10-06 23:04:32 +0300

    Fixed bug: Not ending if the Hive was drop last

--- objects/Hive.gd
+++ objects/Hive.gd
@@ -4,7 +4,7 @@ onready var dropped = false
 onready var desc = "These bees seem friendly."
 
 func on_Click():
-	if !$Buzz.playing and !dropped and allowed():
+	if !dropped and allowed():
 		dropped = true
 		$Buzz.play()
 		get_parent().get_node("Anime").play("Hive")
